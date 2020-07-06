b307cdb91aaf0069b6dbdcc6204f9d422fa6be17
vysakh <vyshuks@gmail.com>
2020-07-06 14:04:43 +0530

    Day 2

--- binary_tree_level_order.py
+++ binary_tree_level_order.py
@@ -16,3 +16,32 @@ return its bottom-up level order traversal as:
   [3]
 ]
 """
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+        result = []
+        if root is None:
+            return result
+
+        Q = []
+        Q.append(root)
+        while len(Q) > 0:
+            nodes = []
+            for i in range(len(Q)):
+                node = Q.pop(0)
+                nodes.append(node.val)
+                if node.left != None:
+                    Q.append(node.left)
+                if node.right != None:
+                    Q.append(node.right)
+
+            result.insert(0, nodes)
+
+        return result
